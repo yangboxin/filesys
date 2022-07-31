4e0f2ed838993bb512749528ea953b234b400c70
paperdoger <76141918+paperdoger@users.noreply.github.com>
2022-07-31 14:29:27 +0800

    Update segment.h

--- segment.h
+++ segment.h
@@ -377,7 +377,7 @@ static inline unsigned int get_valid_time_blocks(struct f2fs_sb_info *sbi,
 		wcount += get_seg_entry(sbi, start + i)->wflag;
 
 	mtime = div_u64(mtime, usable_segs_per_sec);
-	wcount = div_u64(wcount, usable_segs_per_sec);
+	wcount = div_u64(wcount, usable_segs_per_sec)*div_u64(get_valid_blocks(sbi,segno,true));
 
 	u = (wcount * 5 * 100) >> sbi->log_blocks_per_seg+2;//80%
 
